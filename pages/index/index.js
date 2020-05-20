862dc6424256d08092f5a070177c73f948472755
JackChuengQAQ <unknown@example.com>
2020-05-20 17:53:19 +0800

    前端尝试1
    基本了解（抄）了一些功能，目前来说都尚未完全：
    Navigation导航页，用于在测试阶段看到各个用户端的页面
    Tabbar页面无法正常切换，可能需要自定义组件进行操作；
    完成签到日历页面制作，制作完成（配色等可再调整，同时考虑制作为组件而非页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,12 +4,17 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '欢迎使用大大签到',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
+  goToStart: function() {
+    wx.navigateTo({
+      url: 'start',
+    })
+  },
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
