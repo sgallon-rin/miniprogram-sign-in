b7c1ff76263be877b85829da5ac47399a5b0428f
sgallon-rin <shcmsgallon@outlook.com>
2020-07-07 21:49:58 +0800

    更改了libVersion; 增加newchecktest测试云数据库返回
    更改了libVersion; 增加newchecktest测试云数据库返回

--- cloudfunction/stu_info/index.js
+++ cloudfunction/stu_info/index.js
@@ -1,13 +1,13 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
 cloud.init()
 const db = cloud.database()
 const MAX_LIMIT = 100
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   // 先取出集合记录总数
-  const countResult = await db.collection('stusent_info').count()
+  const countResult = await db.collection('student_info').count()
   const total = countResult.total
   // 计算需分几次取
   const batchTimes = Math.ceil(total / 100)
