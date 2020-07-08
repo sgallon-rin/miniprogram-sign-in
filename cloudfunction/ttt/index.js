710ca973a785b7224909fbc6ea920e6b819037b5
sgallon-rin <shcmsgallon@outlook.com>
2020-07-09 00:44:13 +0800

    云函数更新
    云函数更新，教务员条件查询

---
+++ cloudfunction/ttt/index.js
@@ -0,0 +1,23 @@
+/* 
+云函数：stu_info_part.js
+根据传入参数 departmentList, genderList （院系列表、性别列表）
+返回满足条件的学生的信息
+*/
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  env: "dadaqiandao-p86hz"
+})
+const db = cloud.database()
+var _ = db.command
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  //const wxContext = cloud.getWXContext()
+  return cloud.database().collection('student_info')
+  .where({
+    gender: _.in(event.genderList),
+    department: _.in(event.departmentList)
+  })
+  .get()
+}
